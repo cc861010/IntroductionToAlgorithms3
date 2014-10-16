107eb7c9e95e2dcaf89f7222fb745d8d3a2c3735
cc861010 <cc861010@gmail.com>
2014-10-16 18:06:48 +0800

    cc

--- src/com/cc/introduceToAlgorithms/Util.java
+++ src/com/cc/introduceToAlgorithms/Util.java
@@ -1,5 +1,7 @@
 package com.cc.introduceToAlgorithms;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -143,4 +145,66 @@ public class Util {
         }
 
     }
+
+
+
+    private static class Node<T extends Comparable<T>> {
+        T value;
+        Node<T> left, right;
+
+        public void insertToTree(T v) {
+            if (value == null) {
+                value = v;
+                return;
+            }
+            if (v.compareTo(value) < 0) {
+                if (left == null) {
+                    left = new Node<T>();
+                }
+                left.insertToTree(v);
+            } else {
+                if (right == null) {
+                    right = new Node<T>();
+                }
+                right.insertToTree(v);
+            }
+        }
+
+        public void printTree(OutputStreamWriter out) throws IOException {
+            if (right != null) {
+                right.printTree(out, true, "");
+            }
+            printNodeValue(out);
+            if (left != null) {
+                left.printTree(out, false, "");
+            }
+        }
+        private void printNodeValue(OutputStreamWriter out) throws IOException {
+            if (value == null) {
+                out.write("<null>");
+            } else {
+                out.write(value.toString());
+            }
+            out.write('\n');
+        }
+        // use string and not stringbuffer on purpose as we need to change the indent at each recursion
+        private void printTree(OutputStreamWriter out, boolean isRight, String indent) throws IOException {
+            if (right != null) {
+                right.printTree(out, true, indent + (isRight ? "        " : " |      "));
+            }
+            out.write(indent);
+            if (isRight) {
+                out.write(" /");
+            } else {
+                out.write(" \\");
+            }
+            out.write("----- ");
+            printNodeValue(out);
+            if (left != null) {
+                left.printTree(out, false, indent + (isRight ? " |      " : "        "));
+            }
+        }
+    }
+
+
 }
